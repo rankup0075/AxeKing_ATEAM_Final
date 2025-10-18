82c6cc47b924671f9e59daa9decb8f01793738b2
Jeonsmin <wjstnals1115@gmail.com>
2025-10-18 18:12:09 +0900

    미우마오

---
+++ Assets/MissingPrefabCleaner.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MissingPrefabCleaner
+{
+    [MenuItem("Tools/Clean Missing Prefabs In Scene")]
+    static void CleanMissingPrefabs()
+    {
+        int count = 0;
+        var allObjects = GameObject.FindObjectsOfType<GameObject>();
+
+        foreach (var go in allObjects)
+        {
+            if (PrefabUtility.IsPartOfPrefabInstance(go))
+            {
+                var prefabAsset = PrefabUtility.GetCorrespondingObjectFromSource(go);
+                if (prefabAsset == null)
+                {
+                    GameObject.DestroyImmediate(go);
+                    count++;
+                }
+            }
+        }
+
+        Debug.Log($"🧹 Removed {count} missing prefab instance(s) from the scene.");
+    }
+}
